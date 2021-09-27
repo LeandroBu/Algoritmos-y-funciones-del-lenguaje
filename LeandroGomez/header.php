15d87b7f1d728c2b2def37c87da4688582cbefaa
Leandro <leandro.ariel.gomez1@gmail.com>
2021-09-27 02:24:19 -0300

    Armado de proyecto base

---
+++ LeandroGomez/header.php
@@ -0,0 +1,10 @@
+<div class="w3-container w3-blue w3-center">
+        <h1 class="w3-sans-serif">Leandro Gomez</h1>
+    </div>
+
+    <div class="w3-bar w3-black w3-padding">
+        <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Ejercicio 1</a>
+        <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Ejercicio 2</a>
+        <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Ejercicio 3</a>
+        <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Ejercicio 4</a>
+    </div>
---
+++ LeandroGomez/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once 'head.php'
+?>
+<body>
+    
+    <?php
+
+        require_once 'header.php';
+
+    ?>
+
+    <div class="w3-container">
+        <div class="w3-margin w3-center">
+            <iframe src="PDF/TrabajoPracticoGeneral.pdf" height="400" width="80%" ></iframe>
+        </div>
+    </div>
+    
+</body>
+</html>
