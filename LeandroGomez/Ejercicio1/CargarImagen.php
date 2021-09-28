0dcbccbe1cc1e0719324dc86866302ba947029c0
Leandro <leandro.ariel.gomez1@gmail.com>
2021-09-28 01:02:20 -0300

    Primer ejercicio del trabajo practico

---
+++ LeandroGomez/Ejercicio1/CargarImagen.php
@@ -0,0 +1,44 @@
+<?php
+
+$CarpetaDireccion = "imagenes/";
+$ruta_archivo = $CarpetaDireccion ."/". $_FILES["archivo"]["name"];
+$uploadOk = 1;
+//contiene el tipo de imagen que se va a subir
+$Tipo_de_imagen = strtolower(pathinfo($ruta_archivo,PATHINFO_EXTENSION));
+
+
+// Verifico si el archivo existe
+if (file_exists($ruta_archivo)) 
+{
+    echo "No existe el archivo";
+    $uploadOk = 0;
+}
+  
+  // Verifico que el archivo no seas mas del peso permitido
+if ($_FILES["archivo"]["size"] > 500000) 
+{
+    echo "elija un archivo mas liviano";
+    $uploadOk = 0;
+}
+  
+  // Aviso que tipo de formatos no son aceptados
+if($Tipo_de_imagen != "jpg" && $Tipo_de_imagen != "png" && $Tipo_de_imagen != "jpeg"
+  && $Tipo_de_imagen != "gif" ) 
+{
+    echo "No se permite ese tipo de imagen.";
+    $uploadOk = 0;
+}
+  
+  // Aviso de si no se subio el archivo
+if ($uploadOk == 0) 
+{
+    echo "No se subio la imagen";
+  // if everything is ok, try to upload file
+} 
+else 
+{
+    move_uploaded_file($_FILES["archivo"]["tmp_name"], $ruta_archivo);
+}
+
+?>
+
--- LeandroGomez/header.php
+++ LeandroGomez/header.php
@@ -2,8 +2,9 @@
         <h1 class="w3-sans-serif">Leandro Gomez</h1>
     </div>
 
-    <div class="w3-bar w3-black w3-padding">
-        <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Ejercicio 1</a>
+    <div class="w3-bar w3-black">
+        <a href="../index.php" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">HOME</a>
+        <a href="Ejercicio1/Insta-gramo.php" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Insta-gramo</a>
         <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Ejercicio 2</a>
         <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Ejercicio 3</a>
         <a href="#" class="w3-bar-item w3-button w3-hover-none w3-border-black w3-bottombar w3-hover-border-blue w3-hover-text-blue w3-mobile">Ejercicio 4</a>
